9a5be8365efd9be342b16db0720f6507286e8216
ojanafif <ojanafif@gmail.com>
2024-02-28 06:06:40 +0700

    Animation-And-Designing

--- js/script.js
+++ js/script.js
@@ -43,3 +43,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Banner AutoSlides
+var slideIndex = 1;
+showDivs (slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("img-slideShow");
+    if (n > imgList.length) (slideIndex = 1)
+    if (n < 1) {slideIndex = imgList.length};
+    for (i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none";
+    }
+    imgList[slideIndex-1].style.display = "block";
+}
+
+setInterval(() => {
+    plusDivs(1);
+}, 2000);
